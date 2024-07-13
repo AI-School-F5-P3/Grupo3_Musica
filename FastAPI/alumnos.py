7e4544d80489ccdfecc582512de7e94f92c93bae
Aitor Pérez <148784736+aitorph7@users.noreply.github.com>
2024-07-13 18:21:52 +0200

    feat: método get.alumno/{id} con try except

--- FastAPI/alumnos.py
+++ FastAPI/alumnos.py
@@ -15,18 +15,26 @@ class Alumno(BaseModel):
     correo: str
     familiar_id: int
 
-alumnos_fake_db = [Alumno(id=1, nombre="Juan", apellidos="Pérez", edad=25, teléfono="123456789", correo="juan@example.com", familiar_id=2),
-                   Alumno(id=2, nombre="María", apellidos="González", edad=30, teléfono="987654321", correo="maria@example.com", familiar_id=1),
-                   Alumno(id=3, nombre="Pedro", apellidos="Martínez", edad=20, teléfono="456789123", correo="pedro@example.com", familiar_id=3)]
+alumnos_fake_db = [Alumno(id=1, nombre="Juan", apellidos="Pérez", edad=25, teléfono="123456789", correo="juan@ealumnoample.com", familiar_id=2),
+                   Alumno(id=2, nombre="María", apellidos="González", edad=30, teléfono="987654321", correo="maria@ealumnoample.com", familiar_id=1),
+                   Alumno(id=3, nombre="Pedro", apellidos="Martínez", edad=20, teléfono="456789123", correo="pedro@ealumnoample.com", familiar_id=3)]
 
 
 @escuela_musica.get("/alumnosjson")
 async def alumnosjson():
-  return [{"id": 1, "nombre": "Juan", "apellidos": "Pérez", "edad": 25, "teléfono": "123456789", "correo": "juan@example.com", "familiar_id": 2},
-          {"id": 2, "nombre": "María", "apellidos": "González", "edad": 30, "teléfono": "987654321", "correo": "maria@example.com", "familiar_id": 1},
-          {"id": 3, "nombre": "Pedro", "apellidos": "Martínez", "edad": 20, "teléfono": "456789123", "correo": "pedro@example.com", "familiar_id": 3}]
+  return [{"id": 1, "nombre": "Juan", "apellidos": "Pérez", "edad": 25, "teléfono": "123456789", "correo": "juan@ealumnoample.com", "familiar_id": 2},
+          {"id": 2, "nombre": "María", "apellidos": "González", "edad": 30, "teléfono": "987654321", "correo": "maria@ealumnoample.com", "familiar_id": 1},
+          {"id": 3, "nombre": "Pedro", "apellidos": "Martínez", "edad": 20, "teléfono": "456789123", "correo": "pedro@ealumnoample.com", "familiar_id": 3}]
 
 
 @escuela_musica.get("/alumnos")
 async def alumnos():
   return alumnos_fake_db
+
+@escuela_musica.get("/alumno/{id}")
+async def alumno(id : int):
+  alumnos = filter(lambda alumno: alumno.id == id, alumnos_fake_db)
+  try:
+    return list(alumnos)[0]
+  except:
+    return "Alumno no encontrado"
