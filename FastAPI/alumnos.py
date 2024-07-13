1c49bdab82ad3e19d630b3557cc25d07e4bf1b5d
Aitor Pérez <148784736+aitorph7@users.noreply.github.com>
2024-07-13 19:09:15 +0200

    feat: método post.alumno

--- FastAPI/alumnos.py
+++ FastAPI/alumnos.py
@@ -15,36 +15,44 @@ class Alumno(BaseModel):
     correo: str
     familiar_id: int
 
-alumnos_fake_db = [Alumno(id=1, nombre="Juan", apellidos="Pérez", edad=25, teléfono="123456789", correo="juan@ealumnoample.com", familiar_id=2),
-                   Alumno(id=2, nombre="María", apellidos="González", edad=30, teléfono="987654321", correo="maria@ealumnoample.com", familiar_id=1),
-                   Alumno(id=3, nombre="Pedro", apellidos="Martínez", edad=20, teléfono="456789123", correo="pedro@ealumnoample.com", familiar_id=3)]
+alumnos_fake_db = [Alumno(id=1, nombre="Juan", apellidos="Pérez", edad=25, teléfono="123456789", correo="juan@example.com", familiar_id=2),
+                   Alumno(id=2, nombre="María", apellidos="González", edad=30, teléfono="987654321", correo="maria@example.com", familiar_id=1),
+                   Alumno(id=3, nombre="Pedro", apellidos="Martínez", edad=20, teléfono="456789123", correo="pedro@example.com", familiar_id=3)]
 
 
 @escuela_musica.get("/alumnosjson")
 async def alumnosjson():
-  return [{"id": 1, "nombre": "Juan", "apellidos": "Pérez", "edad": 25, "teléfono": "123456789", "correo": "juan@ealumnoample.com", "familiar_id": 2},
-          {"id": 2, "nombre": "María", "apellidos": "González", "edad": 30, "teléfono": "987654321", "correo": "maria@ealumnoample.com", "familiar_id": 1},
-          {"id": 3, "nombre": "Pedro", "apellidos": "Martínez", "edad": 20, "teléfono": "456789123", "correo": "pedro@ealumnoample.com", "familiar_id": 3}]
+  return [{"id": 1, "nombre": "Juan", "apellidos": "Pérez", "edad": 25, "teléfono": "123456789", "correo": "juan@example.com", "familiar_id": 2},
+          {"id": 2, "nombre": "María", "apellidos": "González", "edad": 30, "teléfono": "987654321", "correo": "maria@example.com", "familiar_id": 1},
+          {"id": 3, "nombre": "Pedro", "apellidos": "Martínez", "edad": 20, "teléfono": "456789123", "correo": "pedro@example.com", "familiar_id": 3}]
 
 
 @escuela_musica.get("/alumnos")
 async def alumnos():
   return alumnos_fake_db
 
-# Path
-@escuela_musica.get("/alumno/{id}")
+@escuela_musica.get("/alumno/{id}") # Path parameter
 async def alumno(id : int):
   return buscar_alumno(id)
 
-# Query
-@escuela_musica.get("/alumno/")
+@escuela_musica.get("/alumno/") # Query parameter
 async def alumno(id : int):
   return buscar_alumno(id)
   
-  
+    
+@escuela_musica.post("/alumno/")
+async def alumno_post(alumno: Alumno):
+  if type(buscar_alumno(alumno.id)) == Alumno:
+   return { "error" : "El alumno ya existe" }
+  else: 
+    alumnos_fake_db.append(alumno)
+  return alumno
+
+
+
 def buscar_alumno(id: int):
   alumnos = filter(lambda alumno: alumno.id == id, alumnos_fake_db)
   try:
     return list(alumnos)[0]
   except:
-    return  { "Error" : "Alumno no encontrado" }
+    return  { "error" : "Alumno no encontrado" }
